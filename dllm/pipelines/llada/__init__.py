e71b76ede7a1fad16ef1933be21c408763989b08
Lingjie Chen <lingjie7@idealab-05.cs.illinois.edu>
2025-12-09 11:16:15 -0600

    enable llada2moe sampler

--- dllm/pipelines/llada/__init__.py
+++ dllm/pipelines/llada/__init__.py
@@ -2,3 +2,5 @@ from .models.modeling_llada import LLaDAModelLM
 from .models.configuration_llada import LLaDAConfig
 from .models.modeling_lladamoe import LLaDAMoEModelLM
 from .models.configuration_lladamoe import LLaDAMoEConfig
+from .models.configuration_llada2_moe import LLaDA2MoeConfig
+from .models.modeling_llada2_moe import LLaDA2MoeModelLM
--- dllm/pipelines/llada/models/__init__.py
+++ dllm/pipelines/llada/models/__init__.py
@@ -2,6 +2,8 @@ from .configuration_llada import LLaDAConfig
 from .modeling_llada import LLaDAModelLM
 from .configuration_lladamoe import LLaDAMoEConfig
 from .modeling_lladamoe import LLaDAMoEModelLM
+from .configuration_llada2_moe import LLaDA2MoeConfig
+from .modeling_llada2_moe import LLaDA2MoeModelLM
 
 # Register with HuggingFace Auto classes for local usage
 try:
@@ -14,6 +16,10 @@ try:
     AutoConfig.register("lladamoe", LLaDAMoEConfig)
     AutoModel.register(LLaDAMoEConfig, LLaDAMoEModelLM)
     AutoModelForMaskedLM.register(LLaDAMoEConfig, LLaDAMoEModelLM)
+
+    AutoConfig.register("llada2_moe", LLaDA2MoeConfig)
+    AutoModel.register(LLaDA2MoeConfig, LLaDA2MoeModelLM)
+    AutoModelForMaskedLM.register(LLaDA2MoeConfig, LLaDA2MoeModelLM)
 except ImportError:
     # transformers not available or Auto classes not imported
     pass
